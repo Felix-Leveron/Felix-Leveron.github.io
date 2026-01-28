de7e0058e8e579eed88fe770b24b3e01d76581c9
Felix-Leveron <leveron52798@students.apsb.org>
2026-01-28 15:16:05 +0000

    completed bouncing box project

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -31,19 +31,43 @@ $(function () {
 
 
     // TODO 2 - Create Platforms
-
-
-
-
+createPlatform(400, 150, 20, 600, "purple")
+createPlatform(300, 650, 100, 20, "grey")
+createPlatform(150, 525, 100, 20, "grey")
+createPlatform(150, 400, 100, 20, "grey")
+createPlatform(150, 275, 100, 20, "grey")
+createPlatform(300, 150, 100, 20, "grey")
+createPlatform(600, 0, 20, 650, "purple")
+createPlatform(600, 630, 250, 20, "grey")
+createPlatform(1000, 650, 100, 20, "grey")
+createPlatform(1250, 590, 100, 20, "grey")
+createPlatform(1000, 500, 100, 20, "grey")
+createPlatform(830, 300, 20, 330, "purple")
+createPlatform(1100, 280, 300, 20, "grey")
+createPlatform(950, 380, 100, 20, "grey")
+createPlatform(675, 250, 100, 20, "grey")
+createPlatform(1200, 150, 100, 20, "grey")
+createBadPlatform(620, 610, 210, 20, "red")
+createBadPlatform(0, 0, 20, 800, "red")
     // TODO 3 - Create Collectables
+createCollectable("diamond", 1230, 100);
+createCollectable("max", 330, 105)
+createCollectable("max", 180, 230)
+createCollectable("max", 180, 355)
+createCollectable("max", 180, 480)
+createCollectable("kennedi", 480, 200)
+createCollectable("kennedi", 480, 300)
+createCollectable("kennedi", 480, 400)
+createCollectable("kennedi", 480, 500)
+createCollectable("kennedi", 480, 600)
+createCollectable("database", 700, 200)
 
-
-
-    
     // TODO 4 - Create Cannons
-
-
-    
+createCannon("left", 560, 1500)
+createCannon("top", 710, 1500)
+createCannon("top", 860, 1500)
+createCannon("right", 330, 2000)
+  
     
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
